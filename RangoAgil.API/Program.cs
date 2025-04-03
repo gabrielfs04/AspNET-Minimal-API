41acb14fd51f8d7435bf2a95dd024fbb46ff4a02
Gabriel Maia <gabrielfs.ti04@gmail.com>
2025-04-03 17:46:33 -0300

    Testes de URL com Get

--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -11,4 +11,22 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("rango/{nome}", (RangoDbContext rangoDbContext, string nome) =>{
+
+    return rangoDbContext.Rangos.FirstOrDefault(x => x.Nome == nome);
+
+});
+
+app.MapGet("/rango/{id:int}", (RangoDbContext rangoDbContext, int id) => {
+
+    return rangoDbContext.Rangos.First(x => x.Id == id);
+
+});
+
+app.MapGet("/rangos", (RangoDbContext rangoDbContext) =>
+{
+    return rangoDbContext.Rangos;
+
+});
+
 app.Run();
