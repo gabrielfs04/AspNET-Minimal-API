832b0588a8404a361598fff80b2d4590fc834599
Gabriel Maia <gabrielfs.ti04@gmail.com>
2025-08-11 14:36:57 -0300

    Inserindo o AutoMapper.

--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -1,5 +1,8 @@
 using RangoAgil.API.DbContexts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using RangoAgil.API.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,25 +10,36 @@ builder.Services.AddDbContext<RangoDbContext>(
     o => o.UseSqlite(builder.Configuration["ConnectionStrings:RangoDbConStr"])
     );
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("rango/{nome}", async (RangoDbContext rangoDbContext, string nome) =>{
+app.MapGet("/rangos", async Task<Results<NoContent, Ok<List<Rango>>>>
+    (RangoDbContext rangoDbContext,
+    [FromQuery(Name = "name")]string? rangoNome) =>{
 
-    return await rangoDbContext.Rangos.FirstOrDefaultAsync(x => x.Nome == nome);
+    var rangosEntity = await rangoDbContext.Rangos.
+                                Where(x => rangoNome == null || x.Nome.ToLower().Contains(rangoNome.ToLower()))
+                                .ToListAsync();
+    if (rangosEntity.Count <= 0 || rangosEntity == null)
+        return TypedResults.NoContent();
+    else
+        return TypedResults.Ok(rangosEntity);
 
+});
+
+app.MapGet("/rangos/{rangoId:int}/ingredientes", async (RangoDbContext rangoDbContext, int rangoId) =>
+{
+    return await rangoDbContext.Rangos
+                               .Include(rango => rango.Ingredientes)
+                               .FirstOrDefaultAsync(rango => rango.Id == rangoId);
 });
 
 app.MapGet("/rango/{id:int}", async(RangoDbContext rangoDbContext, int id) => {
 
     return await rangoDbContext.Rangos.FirstOrDefaultAsync(x => x.Id == id);
-
-});
-
-app.MapGet("/rangos", async (RangoDbContext rangoDbContext) =>
-{
-    return await rangoDbContext.Rangos.ToListAsync();
 
 });
 
